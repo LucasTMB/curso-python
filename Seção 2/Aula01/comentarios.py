c10f1fa7fcca407babb6c162df441d44100b7187
LucasTMB <lucas.tmb708@gmail.com>
2022-09-22 23:03:19 -0300

    Primeiro commit

---
+++ Seção 2/Aula01/comentarios.py
@@ -0,0 +1,11 @@
+# Isso é um comentário
+
+'''
+Esse também é um comentário
+'''
+
+"""
+Esse é mais um comentário, só que com aspas duplas
+"""
+
+print('Isso não é um comentário')
---
+++ Seção 2/Aula01/print.py
@@ -0,0 +1,15 @@
+print('Hello World!') #O print poderá ser usado com aspas simples...
+print("Olá mundo!") #... e também com aspas duplas
+
+print('Lucas', 'Teixeira') #O espaço é colocado entre os argumentos como padrão
+sobrenome = 'Teixeira'
+print('Lucas', sobrenome) #Demonstrando que o espaço realmente é utilizado
+
+print('Spider', 'Man', sep='-') #O 'sep' é um argumento que permite a separação por qualquer tipo de valor inserido
+print('lucas', 'gmail.com', sep='@') #Outro exemplo
+
+print('Lucas está ', end='') #O end permite que você decida oq irá ocorrer após o termino do print
+print('aprendendo Python')
+
+print('824', '176', '070', sep='.', end='-')
+print('18')
