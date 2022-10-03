167c7649cadf1cb40ce2254ec60d71d2b66229b7
LucasTMB <lucas.tmb708@gmail.com>
2022-10-02 22:33:24 -0300

    Adicionando o arquivo da aula sobre string

---
+++ Seção 2/string.py
@@ -0,0 +1,17 @@
+'''
+str - string
+'''
+
+print('Uma string em aspas simples')
+print("Uma string em aspas duplas")
+print(123456) #Int
+print('123456') #String (str)
+
+print("Essa é uma 'string'")
+print('Essa é outra "string"')
+
+print("Estou utilizando um caractere de \"escape\"")
+print('Estou utilizando um caractere de \'escape\'')
+
+print('Vou pular de linha \n agora')
+print(r'O /n não vai funcionar')
