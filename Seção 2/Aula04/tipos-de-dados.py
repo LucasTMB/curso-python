56c9083c6407ea393c3930d1264195aa4b9de73f
LucasTMB <lucas.tmb708@gmail.com>
2022-10-17 22:16:41 -0300

    Adicionando mais aulas

---
+++ Seção 2/Aula04/tipos-de-dados.py
@@ -0,0 +1,27 @@
+"""
+Tipos de dados:
+
+str - string
+int - inteiro
+float - real/ponto flutuante
+bool - booleano/lógico 
+"""
+# conversão
+
+print(type(int('10')))
+print(type(str(10)))
+print(type(float('10')))
+
+# Exercício
+
+# String: nome
+print('Lucas Teixeira', type('Lucas Teixeira'))
+
+# Idade: int
+print(19, type(19))
+
+# Altura: float
+print(1.70, type(1.70))
+
+# É maior de idade: bool
+print(19 >= 18, type(19 >= 18))
---
+++ Seção 2/Aula05/operadores-aritmeticos.py
@@ -0,0 +1,16 @@
+"""
++, -, *, /, //, **, %, ()
+"""
+
+num1 = 10
+num2 = 5
+
+print("Soma: ", num1 + num2)
+print("Subtração: ", num1 - num2)
+print("Multiplicação: ", num1 * num2)
+print("Divisão: ", num1 / num2)
+print("Divisão inteira: ", num1 // num2)
+print("Potenciação: ", num1 ** num2)
+print("Resto: ", num1 % num2)
+
+print(20*'A')
