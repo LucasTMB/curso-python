4ca548200d622122b13ed27aea530ff4eb1ec569
LucasTMB <lucas.tmb708@gmail.com>
2023-01-24 23:53:47 -0300

    Finalizando a atividade

--- Seção 3/Exercicios/iterando_strings_com_while.py
+++ Seção 3/Exercicios/iterando_strings_com_while.py
@@ -1,1 +1,25 @@
 frase = 'O Python é uma linguagem de programação multiparadigma. Python foi criado por Guido van Rossum.'
+
+frase_lower = frase.lower()
+
+#print(frase.lower().count('python'))
+
+i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+while i < len(frase_lower):
+    letra_atual = frase_lower[i]
+
+    if letra_atual == ' ':
+        i += 1
+        continue
+
+    qtd_apareceu_mais_vezes_atual = frase_lower.count(letra_atual)
+
+    if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
+        qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
+        letra_apareceu_mais_vezes = letra_atual
+
+    i += 1
+
+print(f'A letra q apareceu mais vezes foi "{letra_apareceu_mais_vezes}", aparecendo {qtd_apareceu_mais_vezes} vezes')
